edf6e735e95c1851482ee218f2233515af660949
sn526909@dal.ca <sn526909@dal.ca>
2019-05-23 05:25:59 -0700

    fizzbuzz

---
+++ task0.py
@@ -0,0 +1,10 @@
+def fizzbuzz (max_number =101)
+   for i in range (max_mnumber):
+      value = ""
+  if i % 3 == 0: value += "Fizz"
+  if i % 5 == 0: value += "Buzz"
+  for number , r in enumerate (fizzbuzz())
+   print "%s: %s" % (number,r)value
+   
+
+
